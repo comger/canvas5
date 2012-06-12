801d0a36cf000c44d9fc381d0f17a62ed895a1d0
comger <kpages@163.com>
2012-06-12 09:38:49 +0800

    ..

--- canvas5.js
+++ canvas5.js
@@ -15,3 +15,54 @@
  * @addon 2012-05-29
 **/
 
+var UI = Class({
+    opt:undefined,
+    getOpt:function(){
+        return opt;
+    },
+    init:function(_opt){
+        opt = _opt;
+    },
+    draw:function(){
+        //todo
+    },
+    drawing:function(){
+        if(this.draw_move){
+            this.draw_move();
+        }
+    },
+    move:function(to){
+        //todo
+    }
+})
+
+
+var URect = Class(UI,{
+    draw:function(){
+        if(opt.isFill){
+            opt.context.fillRect(opt.from.x,opt.from.y,opt.to.x-opt.from.x,opt.to.y-opt.from.y);
+        }else{
+            opt.context.strokeRect(opt.from.x,opt.from.y,opt.to.x-opt.from.x,opt.to.y-opt.from.y);
+        }
+    },
+    draw_move:function(){
+        opt.context.dashRect(opt.from,opt.to);
+    }
+})
+
+
+var ULine = Class(UI,{
+    draw:function(){
+        opt.context.moveTo(opt.from.x,opt.from.y);
+        opt.context.lineTo(opt.to.x,opt.to.y);
+        opt.context.stroke();
+    },
+    draw_move:function(){
+        opt.context.beginPath();
+        opt.context.moveTo(opt.from.x,opt.from.y);
+        opt.context.lineTo(opt.to.x,opt.to.y);
+        opt.context.stroke();
+        opt.context.closePath();
+    }
+})
+
